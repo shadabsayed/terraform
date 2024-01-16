3dc6d12cede0045709b695e9fa281bb0a09088f3
shadabsayed <144091719+shadabsayed@users.noreply.github.com>
2024-01-16 22:54:27 +0530

    Update secgrp.tf

--- secgrp.tf
+++ secgrp.tf
@@ -1,30 +1,22 @@
-provider "aws" {
-  access_key = 
-  secret_key = 
-  region = "ap-south-1"
-}
-
-resource "aws_security_groups" "public_sg" {
+resource "aws_security_group" "public_sg" {
   name        = "security"
   vpc_id      = aws_vpc.my_vpc.id
   description = "allow ssh"
 
   tags = {
-   Name = "pub_sg"
+    Name = "pub_sg"
   }
-}
 
-ingress {
-    description      = "Allow port 22 from anywhere"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
+  ingress {
+    description = "Allow port 22 from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-    description      = "Allow port 22 from anywhere"
+    description      = "Allow all egress traffic"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -35,35 +27,33 @@ ingress {
 
 # for private ----
 
-resource "aws_security_groups" "private_sg" {
+resource "aws_security_group" "private_sg" {
   name        = "pvt_sg"
   vpc_id      = aws_vpc.my_vpc.id
   description = "port 22 & 80"
 
   tags = {
-   name = "pvt_sg"
+    name = "pvt_sg"
   }
 
   ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-   }
-   /*
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    security_groups  = aws_security_groups.load_sg.id
-   }
-   /*
-   
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-    description      = "all access"
+    description      = "Allow all egress traffic"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -72,38 +62,27 @@ resource "aws_security_groups" "private_sg" {
   }
 }
 
-
 # for load balancer---
 
-resource "aws_security_groups" "load_sg" {
+resource "aws_security_group" "load_sg" {
   name        = "load_sg"
   vpc_id      = aws_vpc.my_vpc.id
   description = "port 80 & 443"
 
   tags = {
-   name = "ALB_sg"
+    name = "ALB_sg"
   }
 
   ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-   }
-   /*
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-   ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    security_groups  = aws_security_groups.private_sg.id
-   }
-   /*
-   
   egress {
-    description      = "all access"
+    description      = "Allow all egress traffic"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -114,25 +93,25 @@ resource "aws_security_groups" "load_sg" {
 
 # for RDS ----
 
-resource "aws_security_groups" "rds_sg" {
+resource "aws_security_group" "rds_sg" {
   name        = "rds_sg"
   vpc_id      = aws_vpc.my_vpc.id
   description = "port 3306"
 
   tags = {
-   name = "rds_group"
+    name = "rds_group"
   }
 
-   ingress {
+  ingress {
     description      = "mysql access"
     from_port        = 3306
     to_port          = 3306
     protocol         = "tcp"
-    security_groups  = aws_security_groups.private_sg.id
-   }
-   
+    security_groups  = [aws_security_group.private_sg.id]
+  }
+
   egress {
-    description      = "all access"
+    description      = "Allow all egress traffic"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
