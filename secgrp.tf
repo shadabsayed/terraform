726ccb6ea37467bfcb42714689c8245b12b77709
shadabsayed <144091719+shadabsayed@users.noreply.github.com>
2024-01-16 18:51:00 +0530

    Update secgrp.tf

--- secgrp.tf
+++ secgrp.tf
@@ -11,6 +11,22 @@ resource "aws_security_group" "project-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     }
 
+  ingress {
+    description      = "Allow port 80 from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  ingress {
+    description      = "Allow port 443 from anywhere"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
   egress {
     description      = "Allow all"
     from_port        = 0
